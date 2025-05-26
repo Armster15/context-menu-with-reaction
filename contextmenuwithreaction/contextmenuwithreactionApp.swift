4ef143e53e2524e79b7f04aecf35f71aba187557
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-05-26 12:01:55 -0700

    Initial Commit

---
+++ contextmenuwithreaction/contextmenuwithreactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  contextmenuwithreactionApp.swift
+//  contextmenuwithreaction
+//
+//  Created by Armaan Aggarwal on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct contextmenuwithreactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
